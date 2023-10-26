7b9b3ff89740dafbdd19e848f652d87c5bda39c1
MaciejSerafin <147827187+MaciejSerafin@users.noreply.github.com>
2023-10-26 21:35:32 +0200

    Add files via upload

---
+++ zad4 produkty.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Product
+{
+    string name;
+    float price;
+};
+
+int main()
+{
+    Product products[10];
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Podaj nazwe produktu: ";
+        cin >> products[i].name;
+        cout << "Podaj cene produktu: ";
+        cin >> products[i].price;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 9 - i; j++)
+        {
+            if (products[j].price < products[j + 1].price)
+            {
+                Product temp = products[j];
+                products[j] = products[j + 1];
+                products[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << products[i].name << " " << products[i].price << endl;
+    }
+}
