26695da2140c4d5c777552328e8b87d6988d9dbd
MaciejSerafin <147827187+MaciejSerafin@users.noreply.github.com>
2023-10-26 21:21:59 +0200

    zad1 nieparzyste<300;1200>

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+// Napisz program, który wypisuje wszystkie liczby nieparzyste z
+//przedziału <300;1200> oddzielone spacją. Po pełnych setkach
+//ma być nie spacja lecz sekwencja „ *** ” (spacja gwiazdki
+//spacja). Po wielokrotnościach 400 ma być bezpośrednie
+//przejście do nowego wiersza (bez spacji lub sekwencji znaków)
+
+using namespace std;
+
+int main()
+{
+    for (int i = 300; i <= 1200; i++) {
+        if (i % 2 != 0) {
+            cout << i << " ";
+        }
+        if (i % 100 == 0) {
+            cout << " *** ";
+        }
+        if (i % 400 == 0) {
+            cout << endl;
+        }
+    }
+    return 0;
+}
