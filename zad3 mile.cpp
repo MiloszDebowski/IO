e3ededb162a85e90321170fcba66179db39f9e63
MaciejSerafin <147827187+MaciejSerafin@users.noreply.github.com>
2023-10-26 21:31:51 +0200

    Add files via upload

---
+++ zad3 mile.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void zamiana(double km, double& ml, double& mm)
+{
+    ml = km * 0.621371;
+    mm = km * 0.539957;
+}
+
+int main()
+{
+    double km, ml, mm;
+    cout << "Podaj odleglosc w kilometrach: ";
+    cin >> km;
+    zamiana(km, ml, mm);
+    cout << km << " km to " << ml << " mil ladowych i " << mm << " mil morskich" << endl;
+    return 0;
+}
