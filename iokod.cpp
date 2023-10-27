1a24e44f949a1dfe4dbb8631a629f963f947a81b
Fifi211 <131053662+Fifi211@users.noreply.github.com>
2023-10-27 14:46:18 +0200

    Add files via upload

---
+++ iokod.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int testchoice;
+	int testnumber[10];
+	string choice1;
+	choice1_:
+	cout << "Wybierz test: ";
+	cin >> testchoice;
+	cout << "Czy napewno chcesz wybrac ten test? y/n lub cos innego aby wyjsc" << endl;
+	cin >> choice1;
+	if (choice1 == "n")
+		goto choice1_;
+	if (choice1 == "y") {
+		int i;
+		for (i = 0; i < 9; i++) {
+			if (testchoice = testnumber[i])
+				goto next;
+			else
+				cout << "Nie ma takiego testu.";
+		}
+	}
+	else
+	cout << "Koniec.";
+next: 
+	cout << "TEST: " << endl; //WYSWIETLANIE TESTU
+	//WYBOR ODPOWIEDZI W TESCIE
+	//ZATWIERDZANIE TESTU
+	//KOMUNIKAT O ZAKONCZENIU
+	
+
+}
