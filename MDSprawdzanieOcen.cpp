91b51cac3598b0d79ad57c149f7391f785ce89f8
MiloszDebowski <147827257+MiloszDebowski@users.noreply.github.com>
2023-11-09 16:57:24 +0100

    Update MDSprawdzanieOcen.cpp

--- MDSprawdzanieOcen.cpp
+++ MDSprawdzanieOcen.cpp
@@ -1,51 +1,70 @@
 #include<iostream>
 using namespace std;
-class Przedmiot;
-class Testy;
+struct Przedmiot;
+struct Test;
 
-class Przedmiot :public Testy {
-public:
-	string nazwa;
-	 Testy test[5];
+struct Test
+{
 
-	
-	Przedmiot()
-	{
-		nazwa = "Przedmiot X";
-		test[0].nazwa = "Test1";
-		test[0].ocena = 5;
-		test[1].nazwa = "Test1";
-		test[1].ocena = 5;
-		test[2].nazwa = "Test1";
-		test[2].ocena = 5;
-		test[3].nazwa = "Test1";
-		test[3].ocena = 5;
-		test[4].nazwa = "Test1";
-		test[4].ocena = 5;
-		
-	}
+    string nazwa;
+    int ocena;
+
+
+};
+
+struct Przedmiot:public Test
+{
+
+    string nazwa;
+    Test testy[5];
+
+    Przedmiot()
+    {
+         nazwa="przedmiot x";
+         for(int i=0;i<5;i++)
+         {
+             testy[i].nazwa="test x";
+             testy[i].ocena=5;
+         }
+    }
+
 };
 
 
-
-
-class Testy {
-
-public:
-	string nazwa;
-	float ocena;
-
-	Testy(){}
-
-};
 int main(void)
 {
-	Przedmiot przedmioty[6];
-	
-	cout << "Wybierz przedmiot: ";
-	for(int i=0;i<6;i++)
-	{
-		cout << i + 1 << ". " << przedmioty[i].nazwa << endl;
-	}
+Przedmiot przedmioty[5];
+int x;
+wybor:
+cout<<"Wybierz przedmiot: "<<endl;
+for(int i=0;i<5;i++)
+{
+    cout<<i+1<<". "<<przedmioty[i].nazwa<<endl;
 
+}
+cin>>x;
+system("cls");
+if(x<1||x>5)
+{
+    cout<<endl<<endl<<"NIE MA TAKIEGO PRZEDMIOTU - WYBIERZ JESZCZE RAZ"<<endl<<endl;
+    goto wybor;
+}
+wybor2:
+cout<<"Wybierz test. Dostepne testy: "<<endl;
+
+for(int i=0;i<5;i++)
+{
+cout<<i+1<<". "<<przedmioty[x].testy[i].nazwa<<endl;
+}
+int y;
+cin>>y;
+system("cls");
+    if(y<1||y>5)
+    {
+        cout<<endl<<endl<<"NIE MA TAKIEGO TESTU - WYBIERZ JESZCZE RAZ"<<endl<<endl;
+        goto wybor2;
+    }
+cout<<"Przedmiot: "<<przedmioty[x].nazwa<<endl;
+    cout<<"Test: "<<przedmioty[x].testy[y].nazwa<<endl;
+    cout<<"Ocena: "<<przedmioty[x].testy[y].ocena<<endl;
 }
