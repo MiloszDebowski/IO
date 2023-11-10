42337fd050893748defe805ddb546a408369968d
Fifi211 <131053662+Fifi211@users.noreply.github.com>
2023-11-10 14:25:24 +0100

    Update Rozwiązywanie Testów F.S.cpp

--- Rozwiązywanie Testów F.S.cpp
+++ Rozwiązywanie Testów F.S.cpp
@@ -3,79 +3,96 @@
 
 using namespace std;
 
-struct Test {
+class Test {
+public:
     string testName;
     string testContent;
+
+    Test() {}
+
+    Test(string name, string content) : testName(name), testContent(content) {}
+
+    void displayTest() {
+        cout << "TEST: " << testContent << endl;
+    }
 };
 
-int main() {
-    struct Test tests[5];
-    tests[0] = {"Test 1", "Tresc testu 1"};
-    tests[1] = {"Test 2", "Tresc testu 2"};
-    tests[2] = {"Test 3", "Tresc testu 3"};
-    tests[3] = {"Test 4", "Tresc testu 4"};
-    tests[4] = {"Test 5", "Tresc testu 5"};
+class TestManager {
+private:
+    Test tests[5];
 
-    int testchoice;
-    string choice1;
-    bool validChoice = true;
-
-    cout << "Witaj w programie testowym!" << endl;
-    cout << "Wybierz test z listy ponizej:" << endl;
-
-    for (int i = 0; i < 5; i++) {
-        cout << i + 1 << ". " << tests[i].testName << endl;
+public:
+    TestManager() {
+        tests[0] = { "Test 1", "Tresc testu 1" };
+        tests[1] = { "Test 2", "Tresc testu 2" };
+        tests[2] = { "Test 3", "Tresc testu 3" };
+        tests[3] = { "Test 4", "Tresc testu 4" };
+        tests[4] = { "Test 5", "Tresc testu 5" };
     }
 
-    cout << "6. Wyjscie" << endl;
+    void displayTests() {
+        cout << "Witaj w programie testowym!" << endl;
+        cout << "Wybierz test z listy ponizej:" << endl;
 
-    do {
-        validChoice = true;
+        for (int i = 0; i < 5; i++) {
+            cout << i + 1 << ". " << tests[i].testName << endl;
+        }
+
+        cout << "6. Wyjscie" << endl;
+    }
+
+    int getTestChoice() {
+        int choice;
         cout << "Wybierz test: ";
-        if (!(cin >> testchoice)) {
-            validChoice = false;
+        while (!(cin >> choice) || choice < 1 || choice > 6) {
             cout << "Nieprawidlowy wybor. Sprobuj ponownie." << endl;
             cin.clear();
             while (cin.get() != '\n')
                 continue;
-        } else if (testchoice < 1 || testchoice > 6) {
-            validChoice = false;
-            cout << "Nie ma takiej opcji. Sprobuj ponownie." << endl;
+            cout << "Wybierz test: ";
         }
-    } while (!validChoice);
+        return choice;
+    }
 
-    if (testchoice == 6) {
-        cout << "Koniec." << endl;
-    } else {
-        cout << "Czy na pewno chcesz wybrac ten test? (y/n)" << endl;
-        cin >> choice1;
-        if (choice1 == "n") {
-            main();
-        } else if (choice1 == "y") {
-            cout << "TEST: " << tests[testchoice - 1].testContent << endl;
+    void runTest(int testChoice) {
+        if (testChoice == 6) {
+            cout << "Koniec." << endl;
+        }
+        else {
+            tests[testChoice - 1].displayTest();
             string answer;
             bool answerConfirmed = false;
+
             do {
                 cout << "Wybierz odpowiedz: ";
                 cin >> answer;
                 string confirm;
                 cout << "Czy na pewno chcesz zatwierdzic odpowiedz? (y/n)" << endl;
                 cin >> confirm;
+
                 if (confirm == "y") {
                     answerConfirmed = true;
                     cout << "Test zakonczony." << endl;
-                } else {
+                }
+                else {
                     cout << "Test nie zostal zatwierdzony. Czy chcesz sprobowac ponownie? (y/n)" << endl;
                     cin >> confirm;
+
                     if (confirm == "n") {
                         cout << "Test nie zostal zatwierdzony. Koniec." << endl;
                         break;
                     }
                 }
             } while (!answerConfirmed);
-        } else
-            cout << "Niepoprawny wybor." << endl;
+        }
     }
+};
+
+int main() {
+    TestManager testManager;
+    testManager.displayTests();
+    int testChoice = testManager.getTestChoice();
+    testManager.runTest(testChoice);
 
     return 0;
 }
